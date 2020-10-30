1d3a4b65b5a0315165a9164915d700c81df05c70
partum <jarringt@uccs.edu>
2020-10-30 12:39:49 -0600

    Restructured most of the program
    I still have to do the poll, but I'm pushing now because it may break something.

--- bot.py
+++ bot.py
@@ -2,60 +2,75 @@
 import os
 import discord, random
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv() # this is the stuff in the .env file
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD') # guild = server
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready(): # this makes the Client wait untill it is ready for action
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+# confirmation that the bot has connected (not a command)
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-    print('Guild Members:')
-    async for member in guild.fetch_members(limit=50): #This works while guild.members does not. I'm guessing it's a cache issue.
-        print(f'- {member.name}')
-
-# so this doesn't work. I'm not sure if it's a permission error or what.
-"""@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )"""
+# creates the image list
 
 
-#from https://www.devdungeon.com/content/make-discord-bot-python
-@client.event
-async def on_message(message): #any action that is a respose to a message should go under here
-    '''# we do not want the bot to reply to itself
-    if message.author == client.user:
-        return'''
+# return random image of dog (from list)
+@bot.command(name='dogo', help='Returns a random picture of a dog')
+async def dog_pic(ctx):
+    # it would be better if I could figure out how to pass doggies to the command instead of creating the list everytime
+    my_file = open("pictures.txt", "r")
+    content = my_file.read()
+    doggies = content.split("\n")
+    my_file.close()
+    response = random.choice(doggies)
+    await ctx.send(response) 
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention} :grinning:'.format(message)
-        await message.channel.send(msg)
+# example command. Delete later.
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-    #there is probably a way to do this from a file, if it gets too long
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+# reply hello [name] 
+@bot.command(name='hello', help='Responds with a greeting')
+async def hello(ctx):
+    response = 'Hello ' + str(ctx.author.mention) + ' :wave:'
+    await ctx.send(response)
+
+#return a fun fact
+@bot.command(name='funfact', help='Returns a "fun" fact')
+async def fun_fact(ctx):
+    # this can be done from a file as well if the list gets too long
     funFacts = [
         'Cherophobia is an irrational fear of fun or happiness.',
         'During your lifetime, you will produce enough saliva to fill two swimming pools.',
-        'Polar bears could eat as many as 86 penguins in a single sitting…',
+        'Polar bears could eat as many as 86 penguins in a single sitting...',
         'The “Windy City” name has nothing to do with Chicago weather',
         'Armadillo shells are bulletproof',
     ]
+    response = random.choice(funFacts)
+    await ctx.send(response)
 
-    if message.content == 'funfact!':
-        response = random.choice(funFacts)
-        await message.channel.send(response)
+'''#from https://www.devdungeon.com/content/make-discord-bot-python
+@client.event
+async def on_message(message): #any action that is a respose to a message should go under here
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
 
     #random image
     my_file = open("pictures.txt", "r")
@@ -75,7 +90,7 @@ async def on_message(message): #any action that is a respose to a message should
         splitAnswers = splitQuestion[1].split( )
         await message.channel.send(splitQuestion[0]+'?\n')
         j = 1
-        for i in splitAnswers:
+        for a in splitAnswers:
             if j == 1:
                 msg = await message.channel.send(":one: " + splitAnswers[j-1])
                 j += 1
@@ -142,7 +157,7 @@ async def on_message(message): #any action that is a respose to a message should
                 await msg.add_reaction('🔟')
                 j += 1
             else:
-                quit()
+                quit()'''
        
 
-client.run(TOKEN)
+bot.run(TOKEN)
